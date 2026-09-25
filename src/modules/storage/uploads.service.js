c6152bbf1bc770b34118500a86ded29dd4db9594
benjaminhuarocgil-prog <benjaminhuarocgil@gmail.com>
2026-09-24 19:14:58 -0500

    las fotos de los platos se suben a cloudinari

--- src/modules/storage/uploads.service.js
+++ src/modules/storage/uploads.service.js
@@ -9,6 +9,7 @@ const IMAGE_TYPES = new Map([
 const ALLOWED_SCOPES = new Set([
   'restaurants/logos',
   'restaurants/banners',
+  'restaurants/dishes',
   'drivers/dni',
   'drivers/licenses',
   'drivers/vehicles',
